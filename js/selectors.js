077f13975bc30ebf554ee4846b4a78c341b95375
Rathi <github email address>
2017-05-29 12:07:23 -0400

    jquery selector

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector() {
+   return $('p')
+}
+
+ function lastImageSelector() {
+   return $('div img:last')
+ }
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+   return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child')
+}
+// Write a function firstListItem
+//  that does not accept any parameters.
+// The function should use a first-child
+ // selector to return the first list item
+// in the ul with the ID pic-list.
